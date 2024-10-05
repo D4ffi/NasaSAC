2d77236d87b53474a4fce1049ce8cb438de2d3ab
Deus R <cabrera8067@ucc.mx>
2024-10-05 13:29:11 -0600

    agregue el modelo de los esteroides

---
+++ NasaSpaceAppChallenge/Models/asteroid.cs
@@ -0,0 +1,10 @@
+﻿namespace NasaSpaceAppChallenge.Models
+{
+    public class asteroid
+    {
+        public string Name { get; set; }
+        public double Diameter { get; set; }
+        public double Velocity { get; set; }
+        public double Distance { get; set; }
+    }
+}
